baac3e98c0ec1459983f0d4cba8d7336b2fe07db
Alex Garin <lxg1992@gmail.com>
2019-09-30 22:01:23 +0000

    Nodemon

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080; // default port 8080
@@ -12,18 +12,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const grs = function generateRandomString(n) {
-  let result           = '';
-  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charactersLength = characters.length;
-  for ( let i = 0; i < n; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
-
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -35,11 +23,11 @@ app.listen(PORT, () => {
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
-})
+});
 
 app.get("/hello", function(req,res) {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -47,19 +35,33 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req,res) => {
   let templateVars =  {
-                        shortURL: req.params.shortURL,
-                        longURL: urlDatabase[req.params.shortURL]
-                      };
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
   res.render("urls_show", templateVars);
 
-})
+});
 
 app.post("/urls", (req, res) => {
   console.log(req.body);
-  res.send("OK");
-})
+  let val = req.body.longURL;
+  let key = grs(6);
+  urlDatabase[key] = val;
+  res.redirect(`/urls/${key}`);
+  console.log(urlDatabase);
+});
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
-
-
-
+const grs = function generateRandomString(n) {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < n; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
