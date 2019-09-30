78fa8e81e8f1882c31bb510e53d29bfdf8149576
Alex Garin <lxg1992@gmail.com>
2019-09-30 21:11:52 +0000

    generateRandomString

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -8,6 +11,18 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const grs = function generateRandomString(n) {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for ( let i = 0; i < n; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,6 +41,10 @@ app.get("/hello", function(req,res) {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req,res) => {
   let templateVars =  {
                         shortURL: req.params.shortURL,
@@ -34,4 +53,13 @@ app.get("/urls/:shortURL", (req,res) => {
   res.render("urls_show", templateVars);
 
 })
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+})
+
+
+
+
 
