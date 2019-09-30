926017130a3343564d29748d191bbe733347769c
Alex Garin <lxg1992@gmail.com>
2019-09-30 19:33:19 +0000

    Added parameter input

--- express_server.js
+++ express_server.js
@@ -26,3 +26,12 @@ app.get("/hello", function(req,res) {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 })
 
+app.get("/urls/:shortURL", (req,res) => {
+  let templateVars =  {
+                        shortURL: req.params.shortURL,
+                        longURL: urlDatabase[req.params.shortURL]
+                      };
+  res.render("urls_show", templateVars);
+
+})
+
